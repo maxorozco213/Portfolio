0a14ea2dc5619051a27e6301f0c9d533cd08ded8
Max <maxorozco213@gmail.com>
2020-07-24 12:49:49 -0700

    Set up API call to public repos on GitHub

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+    repoURL: string = 'https://api.github.com/users/maxorozco213/repos';
+    repoList: any;
 
-  constructor() { }
+    constructor(private httpClient: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.fetchData().then(res => {
+            // this.repoList = res;
+            console.log(this.repoList);
+        }).catch(error => {
+            console.log('API error: ', error);
+        });
+    }
 
+    private async fetchData(): Promise<void> {
+        this.repoList = await this.httpClient.get(this.repoURL).toPromise();
+    }
 }
